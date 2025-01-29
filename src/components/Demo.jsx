7a827d5ac42ed29f589a9871f04de11952b7a390
Rnmoon <aryansunilmoon@gmail.com>
2025-01-30 00:17:06 +0530

    .

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,16 +6,35 @@ const Demo = () => {
     url:'',
     summary:'',
   });
+  const [allArticle,setALLArticle] = useState([])
 
   const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(()=>{
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+
+    if(articlesFromLocalStorage){
+      setALLArticle(articlesFromLocalStorage)
+    }
+  },[])
   const handleSubmit = async(e) =>{
     e.preventDefault();
-    const {data} = await getSummary({articleUrl: article.url});
-    if(data?.summary){
-      const newArticle = {...article,summary: data.summary};
-      setArticle(newArticle)
-      console.log(newArticle);
-      
+    try{
+      const {data} = await getSummary({articleUrl: article.url});
+      if(error){
+        console.error("API ERROR: ",error);
+      }
+      if(data?.summary){
+        const newArticle = {...article,summary: data.summary};
+        const updatedAllArticle = [newArticle,...allArticle]
+        setArticle(newArticle)
+        setALLArticle(updatedAllArticle)
+        
+        localStorage.setItem('articles', JSON.stringify(updatedAllArticle));
+        console.log(newArticle);
+      }
+    }catch(err){
+      console.error("Error while fetching summary: ",err);
     }
   }
   return (
@@ -37,6 +56,36 @@ const Demo = () => {
             ↵
           </button>
       </form>
+      <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticle.map((item,index)=>(
+            <div key={`link-${index}`}
+                onClick={()=>setArticle(item)}
+                className="link_card"
+            >
+              <div className="copy_btn">
+                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+    {/* Display Result */}
+    <div className="my-10 max-w-full flex justify-center items-center">
+          {isFetching ? (
+            <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+          ):error ? (
+            <p className="font-satoshi font-normal text-gray-700">Well, that wasn&apos;t supposed to happen... <br /> <span>{error?.data.error}</span></p>
+          ):(
+            article.summary &&(
+              <div className="flex flex-col gap-3">
+                <h2 className="font-satoshi font-bold text-gray-600 text-xl">Article <span className="blue_gradient">Summary</span></h2>
+                <div className="summary_box">
+                  <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                </div>
+              </div>
+            )
+          )}
     </div>
   </section>
   )
